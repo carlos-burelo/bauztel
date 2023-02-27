01372b77ab3efe591dd326fbfc0db3da4939213f
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-02-27 10:36:40 -0600

    :sparkles: feat: add wrapper component for each page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Landing from './home/components/Landing'
 import LatestProducts from './home/components/LatestProducts'
+import Order from './home/components/Order'
+import _ from './home/home.module.scss'
+
 
 const HomePage: Page = () => {
   return (
-    <>
+    <div className={_.container}>
       <Landing />
       <LatestProducts />
-    </>
+      <Order />
+    </div>
   )
 }
 
