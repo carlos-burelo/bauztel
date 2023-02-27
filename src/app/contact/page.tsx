1017cdb371cdc406849a8cf56c6a6d5d41e8cab3
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-02-27 12:59:55 -0600

    :sparkles: feat: add empty pages for soon improvement

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import _ from './wholesale.module.scss'
+
+const ContactPage: Page = () => {
+  return (
+    <center>
+      <h1>Building... 🏗️</h1>
+    </center>
+  )
+}
+
+export default ContactPage
---
+++ src/app/wholesale/page.tsx
@@ -0,0 +1,11 @@
+import _ from './wholesale.module.scss'
+
+const WholesalePage: Page = () => {
+  return (
+    <center>
+      <h1>Building... 🏗️</h1>
+    </center>
+  )
+}
+
+export default WholesalePage
