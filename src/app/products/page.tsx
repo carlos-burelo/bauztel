4de142f59be5e5e3bd96d18c3b00cd797e3e7942
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-02-27 10:40:06 -0600

    :bug: fix: update infobar styles for adjust searchbar

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,14 +2,16 @@ import { PRODUCTS } from '#/shared/mocks/products'
 import Header from './components/Header'
 import Infobar from './components/Infobar'
 import ProductList from './components/ProductList'
+import _ from './products.module.scss'
+
 
 const ProductsPage: Page = () => {
   return (
-    <>
+    <div className={_.container}>
       <Header />
       <Infobar />
       <ProductList list={PRODUCTS} />
-    </>
+    </div>
   )
 
 }
